1919917f0db82dee1742f6c181c3da984c7a528d
MustafaTunahanTuna <mustafatunahantuna@yandex.com>
2021-05-02 03:19:37 +0300

    Yetenekleri listeleme ve yetenek silme fonksiyonları eklendi

---
+++ MvcCvProject/Controllers/SkillController.cs
@@ -0,0 +1,28 @@
+﻿using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCvProject.Controllers
+{
+    public class SkillController : Controller
+    {
+       
+
+        SkillRepository repo = new SkillRepository();
+        public ActionResult Index()
+        {
+            var skills = repo.List();
+            return View(skills);
+        }
+        public ActionResult SkillDelete(int id)
+        {
+            var skill = repo.Find(x => x.ID == id);
+            repo.TDelete(skill);
+            return RedirectToAction("Index");
+        }
+    }
+}
